2cd0e4f0546a33b9d0d884f77b8ca88c6005935a
Cyro Bergonzi <cyro22@hotmail.com>
2019-09-22 18:11:39 -0300

    correções

--- GOLCrudAirPlane.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GOLCrudAirplane.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
